1447ed07feac10805e9664eea1924409a9056c69
Slayermd <47303051+mstrcoder@users.noreply.github.com>
2021-02-16 23:13:53 +0530

    My _Trip API finished

---
+++ a.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+ #define ll long long
+#define pb push_back
+ #define f(i, b) for (ll i = 0; i < b; i++)
+ #define fr(i, a, b) for (int i = a; i >= b; i--)
+#define vt vector
+ #define all(v) v.begin(), v.end()
+ const ll M = 998244353;
+using namespace std;
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "rt", stdin);
+        freopen("output.txt", "wt", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        ll n;
+        ll max1=0;
+        cin>>n;
+        ll a[n];f(i,n)cin>>a[i],max1=max(max1,a[i]);
+        priority_queue<ll>p1,p2;
+
+
+
+        
+
+ 
+ 
+    }
+    #ifndef ONLINE_JUDGE
+        cout<<"\nTime Elapsed : " << 1.0*clock() / CLOCKS_PER_SEC << " s\n";
+    #endif
+    return 0;
+}

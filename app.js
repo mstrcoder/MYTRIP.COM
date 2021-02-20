a157cb0575de1291e3bb7313f753aa547d2d5ffc
Slayermd <47303051+mstrcoder@users.noreply.github.com>
2021-02-20 23:53:48 +0530

    We have succesfully got the error

--- app.js
+++ app.js
@@ -4,30 +4,53 @@ const app = express();
 const morgan = require("morgan");
 const tour = require("./controllers/tour");
 const user = require("./controllers/user");
-const querystring = require('querystring');
+const querystring = require("querystring");
+const AppError=require('./utilities/apperror')
 // const {GetAllTour,CreateNewTour,GetOneTour,UpdateOneTour,DeleteOneTour} = require('./tour');
 //used to add iddleware
 app.use(express.json());
 app.use(express.static(`${__dirname}/starter/public`));
-app.route('/api/v1/tours/top-5-cheap').get(tour.topfivecheapesttour,tour.GetAllTour);
-
+app
+  .route("/api/v1/tours/top-5-cheap")
+  .get(tour.topfivecheapesttour, tour.GetAllTour);
 
 //Aggregration
-app.route("/api/v1/tours/tours-stats").get(tour.getTourStats)
+app.route("/api/v1/tours/tours-stats").get(tour.getTourStats);
 
-app.route("/api/v1/tours/tours-yearly/:year").get(tour.getMonthlyPlan)
+app.route("/api/v1/tours/tours-yearly/:year").get(tour.getMonthlyPlan);
 
 app.route("/api/v1/tours").get(tour.GetAllTour).post(tour.CreateNewTour);
 
+app
+  .route("/api/v1/tours/:id")
+  .get(tour.GetOneTour)
+  .patch(tour.UpdateOneTour)
+  .delete(tour.DeleteOneTour);
 
-app.route("/api/v1/tours/:id").get(tour.GetOneTour).patch(tour.UpdateOneTour).delete(tour.DeleteOneTour);
+app.all("*", (req, res,next) => {
+  // res.status(404).json({
+  //     status:"Failed!",
+  //     message:"Cannot get the Request route"
+  // })  
+  next(new AppError('Cannot Find The Route',400));
+//   const err = new Error('Cannot get the Request route');
+//   err.statusCode = 404;
+//   err.status = 'fail';
+//   next(err);
+});
 
-app.all('*',(req,res)=>{
-    res.status(404).json({
-        status:"Failed!", 
-        message:"Cannot get the Request route"
-    })
-})
+//GLOBAL Middle ware habdler
+app.use((err, req, res, next) => {
+    // console.log("hello3");
+    // console.log(err.status,err.message);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+//   console.log(err.status,err.message);
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 // // app.route("/api/v1/tours/:id").get(tour.GetOneTour).patch(tour.UpdateOneTour);
 // app
 //   .route("/api/v1/tours/:id")

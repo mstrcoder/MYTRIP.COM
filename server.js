b64b78bef2f78b10437ad4494a95ea88de7f13f2
Slayermd <47303051+mstrcoder@users.noreply.github.com>
2021-03-03 00:44:41 +0530

    ADDED TO HEROK  U

--- server.js
+++ server.js
@@ -32,9 +32,19 @@ mongoose
 //   .catch((err) => {
 //     console.log("error aagya!  ", err);
 //   });
-const port=process.env.PORT|5000;
-const server=app.listen(port, () => {
-  console.log("server listening on Port 5000");
+// const port=process.env.PORT|5000;
+// const server=app.listen(port, () => {
+//   console.log("server listening on Port 5000");
+// });
+// var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+// var server_host = process.env.YOUR_HOST || '0.0.0.0';
+// app.listen(server_port, server_host, function() {
+//     console.log('Listening on port %d', server_port);
+// });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
 
 process.on("unhandledRejection", (err) => {
